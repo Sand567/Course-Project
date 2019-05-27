ac6796e9c2f63d7087027aee7abd22dbbceed64d
Sandeep Nagendra <sandynagendra@gmail.com>
2019-05-27 15:27:36 -0400

    Updated and refactored code

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -9,14 +8,21 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe("Test recipe", "This is simply a test recipe", "https://images-gmi-pmc.edge-generalmills.com/2acc07c5-d18e-4157-82b4-da96865f1f00.jpg"),
-    new Recipe("Test recipe", "This is simply a test recipe", "https://images-gmi-pmc.edge-generalmills.com/2acc07c5-d18e-4157-82b4-da96865f1f00.jpg")
+    new Recipe("Mayo Rice", "This is simply a test recipe 1", "https://hispanickitchen.com/wp-content/uploads/2017/07/Ensalada-de-Pollo_1500-700x500.jpg"),
+    new Recipe("Vegetable Biryani", "This is simply a test recipe 2", "https://www.kitchentreaty.com/wp-content/uploads/2015/11/biryani-inspired-autumn-vegetable-rice-with-butternut-squash-cauliflower-and-cashews5-700x500.jpg"),
+    new Recipe("Chick peas and Rice", "This is simply a test recipe 3", "https://www.foodforfitness.co.uk/wp-content/uploads/2017/08/brazilian-picadillo-recipe-horizontal-e1504096062461-700x500.jpg")
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipeElement: Recipe) {
+    this.recipeWasSelected.emit(recipeElement);
   }
 
 }
